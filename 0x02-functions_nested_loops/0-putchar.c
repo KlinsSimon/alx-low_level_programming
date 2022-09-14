f1db08794f72e01887971de4ace56b6795087e65
KlinsSimon <simonmuliklins@gmail.com>
2022-09-14 05:30:01 -0700

    0.putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "_putchar"
+
+/**
+ *  main - print _putchar, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar( 'a');
+	_putchar( 'r' );
+	_putchar('\n');
+
+
+	return (0);
+
+}
+	
